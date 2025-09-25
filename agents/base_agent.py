80e76b324bd0beecd9e01d170bf670b10b0422c8
VT415204 <17964613+ramonuchoa386@users.noreply.github.com>
2025-09-25 14:43:36 -0300

    feat: remove ollama base agent

--- agents/base_agent.py
+++ agents/base_agent.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_ollama import OllamaLLM
 from models.schemas import AgentState
 import os
 
@@ -9,11 +8,6 @@ class BaseAgent(ABC):
         self.gemini_llm = ChatGoogleGenerativeAI(
             model="gemini-2.0-flash",
             google_api_key=os.getenv("GEMINI_API_KEY")
-        )
-        
-        self.local_llm = OllamaLLM(
-            model="codellama:7b",
-            base_url=os.getenv("OLLAMA_BASE_URL", "http://ollama:11434")
         )
     
     @abstractmethod
